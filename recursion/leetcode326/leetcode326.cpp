3802d67cac5788c59553932f3c14d8b26da5a3f6
samssjubair <samssjubair@gmail.com>
2023-03-16 22:22:43 +0600

    recursive way of isPowerOfThree

--- recursion/leetcode326/leetcode326.cpp
+++ recursion/leetcode326/leetcode326.cpp
@@ -11,7 +11,15 @@ bool isPowerOfThree(int n){
             break;
         }
         
-        // cout<< n;
+    }
+    return false;
+}
+
+//recursive way
+bool recursiveIsPowerOfThree(int n){
+    if(n==1) return true;
+    if(n%3==0){
+        return isPowerOfThree(n/3);
     }
     return false;
 }
