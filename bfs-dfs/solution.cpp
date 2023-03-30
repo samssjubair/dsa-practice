a815089145d31baa61906da86489f2eb7399b283
samssjubair <samssjubair@gmail.com>
2023-03-30 23:58:17 +0600

    dfs added

---
+++ bfs-dfs/solution.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+const int N=1e5+2;
+vector<int> g[N];
+bool vis[N];
+
+void dfs(int source){
+    vis[source]=true;
+    cout<<source<<" ";
+    for(int child : g[source]){
+        if(!vis[child]){
+            dfs(child);
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    dfs(1);
+}
--- leetcode567/solution.cpp
+++ leetcode567/solution.cpp
@@ -3,35 +3,34 @@
 #include<vector>
 using namespace std;
 
-bool checkInclusion(string s1, string s2) {
-    if(s1.length() > s2.length()) return false;
-
-        // Create a vector to store the frequency of each character in s1
-        vector<int> count(26, 0);
-        for(int i=0;i<s1.length();i++) {
-            count[s1[i]-'a']++; // Update the frequency count of the character s1[i]
-        }
-
-        // Initialize two pointers to slide the window and a length variable
-        int left = 0, right = 0, len = s1.length();
-        while(right < s2.length()) {
-            // If the character at the right pointer is in s1
-            if(count[s2[right]-'a'] > 0) {
-                count[s2[right]-'a']--; // Decrement the frequency count of the character
-                len--; // Decrement the length variable
-                right++; // Move the right pointer to the right
-            } else {
-                // If the character at the left pointer is not in s1
-                count[s2[left]-'a']++; // Increment the frequency count of the character
-                len++; // Increment the length variable
-                left++; // Move the left pointer to the right
-            }
-            // If we have found a permutation of s1 in s2
-            if(len == 0) return true;
-        }
-        // If we have not found a permutation of s1 in s2
-        return false;
+bool areMapsEqual(map<char,int> a, map<char,int> b){
+    for(char ch='a';ch<='z';ch++){
+        if(a[ch]!=b[ch])
+            return false;
     }
+    return true;
+}
+
+bool checkInclusion(string s1, string s2) {
+    map<char,int>mp1;
+    for(char ch : s1){
+        mp1[ch]++;
+    }
+    int i=0,j=0;
+    map<char,int>mp2;
+    while(j<s2.size()){
+        mp2[s2[j]]++;
+        if(j-i+1==s1.size()){
+            if(areMapsEqual(mp1,mp2)) return true;
+            mp2[s2[i]]--;
+            i++;
+        }
+
+        j++;
+    }
+    return false;
+
+}
     
 
 
